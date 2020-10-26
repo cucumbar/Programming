26705308f2a156b5156fd50f8f8c411cfc384f19
cucumbar <71368434+cucumbar@users.noreply.github.com>
2020-10-26 21:00:49 +0300

    Update 16.py

--- Practice/16/Python/16.py
+++ Practice/16/Python/16.py
@@ -1,5 +1,5 @@
 import re 
-pattern = r"a\w\d\d55661"
+pattern = r"a(\w\d\d|\d\w\d|\d\d\w)55661"
 Siegesmund_tickets = input()
 answer = re.findall(pattern, input())
 print(' '.join(answer) if answer != [] else -1)
